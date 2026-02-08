da5b940c34ed1c6f183e3ba5f0649e7f2317b352
yasminegamal19 <yasminegamal2222@gmail.com>
2026-02-08 13:48:20 +0200

    disable CI warnings for Vercel build

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
 import ListUsers from './components/List_Users/ListUsers';
 import CreateUser from './components/create_user/CreateUser';
